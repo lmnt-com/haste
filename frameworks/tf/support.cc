393d266b2907dfbecfcf482a6dc1d7aeedb44567
Sharvil Nanavati <sharvil.nanavati@gmail.com>
2020-03-27 21:13:51 -0700

    Fix invalid cublasHandle_t use across devices (TensorFlow).
    TensorFlow doesn't pin worker threads to GPU devices. A single thread
    can service multiple GPU devices over the course of a single training
    session.
    
    This change creates a new cublasHandle_t for each (GPU, thread) pair
    instead of creating one per thread. Haste can now be used with
    TensorFlow in a multi-GPU configuration (e.g. MirroredStrategy).

--- frameworks/tf/support.cc
+++ frameworks/tf/support.cc
@@ -17,26 +17,45 @@
 #include <mutex>
 #include <thread>
 #include <unordered_map>
+#include <utility>
 
 #include "support.h"
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/util/stream_executor_util.h"
 #include "tensorflow/stream_executor/stream.h"
 
+using Key = std::pair<int, std::thread::id>;
+
+namespace std {
+
+template<>
+struct hash<Key> {
+  size_t operator()(const Key& key) const noexcept {
+    const auto h1 = std::hash<Key::first_type>{}(key.first);
+    const auto h2 = std::hash<Key::second_type>{}(key.second);
+    return h1 ^ (h2 << 1);
+  }
+};
+
+}
+
 // LOL.
 cublasHandle_t GetCublasHandle(tensorflow::OpKernelContext* context) {
-  static std::unordered_map<std::thread::id, cublasHandle_t> handle_map;
+  static std::unordered_map<Key, cublasHandle_t> handle_map;
   static std::mutex mutex;
 
+  int device;
   std::lock_guard<std::mutex> lock(mutex);
   std::thread::id tid = std::this_thread::get_id();
+  cudaGetDevice(&device);
   cudaStream_t stream = GetCudaStream(context);
-  auto i = handle_map.find(tid);
+  auto item = std::make_pair(device, tid);
+  auto i = handle_map.find(item);
   if (i == std::end(handle_map)) {
     cublasHandle_t handle;
     cublasCreate(&handle);
     cublasSetStream(handle, stream);
-    i = handle_map.insert(std::make_pair(tid, handle)).first;
+    i = handle_map.insert(std::make_pair(item, handle)).first;
   }
 
   return i->second;
