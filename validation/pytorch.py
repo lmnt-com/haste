2170b76d7aae0e2bd0206ce809844d3748c021d8
manipopopo <pwmutantbread@gmail.com>
2022-01-10 12:31:07 +0000

    Add pytorch RNNs with zoneout tests

--- validation/pytorch.py
+++ validation/pytorch.py
@@ -14,6 +14,7 @@
 # ==============================================================================
 
 import argparse
+from unittest import mock
 import torch
 import haste_pytorch as haste
 
@@ -40,16 +41,21 @@ hidden_size = 256
 
 def self_consistency(rnn, x):
   x_cuda = x.clone().cuda()
-  x_cpu = x.clone().cpu()
+  x_cuda_torch = x_cuda.detach().clone()
   x_cuda.requires_grad_(True)
-  x_cpu.requires_grad_(True)
+  x_cuda_torch.requires_grad_(True)
 
-  y1, _ = rnn.cuda().forward(x_cuda)
+  rnn.cuda()
+  seed = 5566
+  torch.manual_seed(seed)
+  y1, _ = rnn.forward(x_cuda)
   y1.backward(torch.ones_like(y1))
-  y2, _ = rnn.cpu().forward(x_cpu)
+  torch.manual_seed(seed)
+  with mock.patch.object(rnn, "_is_cuda", lambda: False):
+    y2, _ = rnn.forward(x_cuda_torch)
   y2.backward(torch.ones_like(y2))
 
-  g1 = x_cpu.grad.data
+  g1 = x_cuda_torch.grad.data
   g2 = x_cuda.grad.data
 
   print(torch.max(torch.abs(y1.cpu()-y2.cpu())))
@@ -83,12 +89,17 @@ def native_consistency(haste_rnn, pytorch_rnn, x):
   print(torch.max(torch.abs(g1-g2)))
 
 
-def run_rnn(rnn_type, x):
-  rnn = rnn_type(input_size, hidden_size)
+def _run_rnn(rnn_type, x, **kwargs):
+  rnn = rnn_type(input_size, hidden_size, **kwargs)
   self_consistency(rnn, x)
-  if rnn_type in HASTE_TO_NATIVE:
+  if rnn_type in HASTE_TO_NATIVE and not kwargs:
     pytorch_rnn = HASTE_TO_NATIVE[rnn_type](input_size, hidden_size)
     native_consistency(rnn, pytorch_rnn, x)
+
+
+def run_rnn(rnn_type, x):
+  for kwargs in [dict(), dict(zoneout=0.5)]:
+    _run_rnn(rnn_type, x, **kwargs)
 
 
 def main(args):
